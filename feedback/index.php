9cd509e7c073067b72d930c40e7a24843c548bc5
VenkataSriSaiSuryaMandava <ssurya.mandava@gmail.com>
2023-12-19 23:33:20 +0530

    my comment

--- feedback/index.php
+++ feedback/index.php
@@ -53,7 +53,7 @@ if (isset($_POST['submit'])) {
 }
 ?>
 
-    <img src="/php-crash/feedback/img/phplogo.png" class="w-25 mb-3" alt="">
+    <img src="/php-crash/feedback/img/php_logo.png" class="w-25 mb-3" alt="">
     <h2>Feedback</h2>
     <?php echo isset($name) ? $name : ''; ?>
     <p class="lead text-center">Leave feedback for PHP Course</p>
