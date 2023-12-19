aa8d27ae21438fe76d0bbb3eb64a5fc86e95faa9
VenkataSriSaiSuryaMandava <ssurya.mandava@gmail.com>
2023-12-19 23:11:25 +0530

    first commit

---
+++ feedback/about.php
@@ -0,0 +1,41 @@
+<?php include 'inc/header.php'; ?>
+  
+    <h1>About our website</h1>
+
+    <div class="card my-3 w-75">
+        <div class="card-body text-center">
+            <h2>Who we are</h2>
+            <div class="text-secondary mt-2">
+                Welcome to our website! We are a team of passionate individuals dedicated to providing valuable information about PHP for students.
+            </div>
+        </div>
+    </div>
+
+    <div class="card my-3 w-75">
+        <div class="card-body text-center">
+            <h2>Our Mission</h2>
+            <div class="text-secondary mt-2">
+                Our mission is to help students to learn PHP. We strive to explain every concept clearly so that the student can understand and apply them in real-time applications.
+            </div>
+        </div>
+    </div>
+
+    <div class="card my-3 w-75">
+        <div class="card-body text-center">
+            <h2>Contact us</h2>
+            <div class="text-secondary mt-2">
+            If you have any querries, feel free to contact us at <a href="mailto:learnphp@gmail.com">learnphp@gmail.com</a>.
+            </div>
+        </div>
+    </div>
+
+    <div class="card my-3 w-75">
+        <div class="card-body text-center">
+            <h2>Feedback</h2>
+            <div class="text-secondary mt-2">
+            If you need to add feedback  click <a href="index.php">Feedback</a>.
+            </div>
+        </div>
+    </div>
+
+<?php include 'inc/footer.php'; ?>
